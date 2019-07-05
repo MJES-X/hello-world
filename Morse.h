a76d9d4f89d8e6f40456ea3fca03c695a559d6f2
王毅 <52456191+MJES-X@users.noreply.github.com>
2019-07-05 11:03:28 +0800

    Update Morse.h

--- Morse.h
+++ Morse.h
@@ -11,60 +11,134 @@ class Morse
 };
 #endif /*_MORSE_H*/
     
-String characters=""; 
-String morse="";
-void setup() 
+class Morse
 {
- Serial.begin(9600);
-}
-char arry[][4]={
-  {'.','-','*','*'},
-  {'-','.','.','.'},
-  {'-','.','-','.'},
-  {'-','.','.','*'},
-  {'.','*','*','*'},
-  {'.','.','-','.'},
-  {'-','-','.','*'},
-  {'.','.','.','.'},
-  {'.','.','*','*'},
-  {'.','-','-','-'},
-  {'-','.','-','*'},
-  {'.','-','.','.'},
-  {'-','-','*','*'},
-  {'-','.','*','*'},
-  {'-','-','-','*'},
-  {'.','-','-','.'},
-  {'-','-','.','-'},
-  {'.','-','.','*'},
-  {'.','.','.','*'},
-  {'-','*','*','*'},
-  {'.','.','-','*'},
-  {'.','.','.','-'},
-  {'.','-','-','*'},
-  {'-','.','.','-'},
-  {'-','.','-','-'},
-  {'-','-','.','.'},
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+    void c_space();
+    void w_space();
+  private:
+    int _pin;
+    int _dottime;
 };
+
+Morse::Morse(int pin)
+{
+	pinMode(pin,OUTPUT);
+	_pin=pin;
+	_dottime=250;
+}
+
+void Morse::dot()
+{
+	digitalWrite(_pin,HIGH);
+	delay(_dottime);
+	digitalWrite(_pin,LOW);
+	delay(_dottime);
+}
+
+void Morse::dash()
+{
+	digitalWrite(_pin,HIGH);
+	delay(_dottime*3);
+	digitalWrite(_pin,LOW);
+	delay(_dottime);
+}
+
+void Morse::c_space()
+{
+	digitalWrite(_pin,LOW);
+	delay(_dottime*4);
+}
+
+void Morse::w_space()
+{
+	digitalWrite(_pin,LOW);
+	delay(_dottime*6);
+}
+
+Morse morse(13);
+char MORSE[][4] = {
+  {'.', '-', '*', '*'}, 
+  {'-', '.', '.', '.'}, 
+  {'-', '.', '-', '.'}, 
+  {'-', '.', '.', '*'}, 
+  {'.', '*', '*', '*'}, 
+  {'.', '.', '-', '.'}, 
+  {'-', '-', '.', '*'}, 
+  {'.', '.', '.', '.'}, 
+  {'.', '.', '*', '*'}, 
+  {'.', '-', '-', '-'}, 
+  {'-', '.', '-', '*'}, 
+  {'.', '-', '.', '.'}, 
+  {'-', '-', '*', '*'}, 
+  {'-', '.', '*', '*'}, 
+  {'-', '-', '-', '*'}, 
+  {'.', '-', '-', '.'}, 
+  {'-', '-', '.', '-'}, 
+  {'.', '-', '.', '*'}, 
+  {'.', '.', '.', '*'}, 
+  {'-', '*', '*', '*'}, 
+  {'.', '.', '-', '*'}, 
+  {'.', '.', '.', '-'}, 
+  {'.', '-', '-', '*'}, 
+  {'-', '.', '.', '-'}, 
+  {'-', '.', '-', '-'}, 
+  {'-', '-', '.', '.'}, 
+  {'-', '-', '-', '*'},
+  {'*', '-', '*', '-'} 
+};
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
 void loop()
-{ 
-  int n=0;
-  while (Serial.available()>0)  
+{
+  String characters = "";
+  String chars = "";
+  int k = 0;
+  int n = 0;
+  while(Serial.available()>0)
+  {
+    k = 1;
+    characters += char(Serial.read());
+    delay(1); 
+    n++;
+  }
+
+  if(k)
+  {
+    for(int i=0;i<n;i++)
     {
-        characters+=char(Serial.read());
-        delay(2);
-        n++;
-    }
-   for (int i=0;i<n;i++)
-    {
-     for (int j=0;j<4;j++)
+      for(int j=0;j<4;j++)
       {
-        if (characters[i]>=97&&characters[i]<=122)
-          {
-            morse += char(arry[int(characters[i]-97)][j]);
-          }
-          morse += ' ';
+        if((characters[i]>=97&&characters[i]<=122))
+        {
+          chars += char(MORSE[int(characters[i]-97)][j]);
+        }
+        if(characters[i]==13)
+        {
+          chars += char(MORSE[27][j]);
+        }
+        if(characters[i]==32)
+        {
+         chars += char(MORSE[28][j]);
+        }
       }
-      Serial.println(morse);
+      chars += ' ';
+    }
+    Serial.println(chars);
+    for(int i = 0;chars[i]!='\0' ; i++)
+    {
+      if(chars[i] == '.')morse.dot();
+      else if(chars[i] == '-')morse.dash();
+      else if(chars[i] == ' ')morse.w_space();
+      if (chars[i] != ' ' && characters[i] != '*')morse.c_space();
     }
     delay(2);
+  }
 }
